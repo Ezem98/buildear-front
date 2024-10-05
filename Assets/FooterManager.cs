188f99d4625733a0081b7bbb99ad15d826d7513d
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-10-04 21:55:35 -0300

    Profile + Favorites

---
+++ Assets/FooterManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FooterManager : MonoBehaviour
+{
+    [SerializeField] Button homeButton;
+    [SerializeField] Button catalogueButton;
+    [SerializeField] Button favoritesButton;
+    [SerializeField] Button profileButton;
+    private Dictionary<string, Button> buttonDictionary;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        buttonDictionary = new()
+        {
+            { "Home", homeButton },
+            { "Catalogue", catalogueButton },
+            { "Favorites", favoritesButton },
+            { "Profile", profileButton }
+        };
+    }
+    void OnEnable()
+    {
+
+    }
+}
