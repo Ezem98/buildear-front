046025496e5b7c5ce35754a0294951fffc1a8877
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-10-29 17:22:11 -0300

    move fence prefab + add functionality to build button

--- Assets/FooterManager.cs
+++ Assets/FooterManager.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Utilities.Extensions;
 
 public class FooterManager : MonoBehaviour
 {
@@ -9,6 +12,7 @@ public class FooterManager : MonoBehaviour
     [SerializeField] Button catalogueButton;
     [SerializeField] Button favoritesButton;
     [SerializeField] Button profileButton;
+    [SerializeField] Button buildButton;
     private Dictionary<string, Button> buttonDictionary;
     // Start is called before the first frame update
 
@@ -28,6 +32,15 @@ public class FooterManager : MonoBehaviour
         {
             buttonDictionary["Profile"].interactable = false;
             buttonDictionary["Favorites"].interactable = false;
+            buildButton.interactable = false;
+        }
+        else if (UIController.Instance.MyModelsData?.Count > 0)
+        {
+            ModelData modelData = UIController.Instance.MyModelsData.Last();
+            UIController.Instance.CurrentModelIndex = modelData.id;
+            UIController.Instance.objectSpawner.spawnOptionId = modelData.id;
+            UIController.Instance.ModelData = modelData;
+            buildButton.interactable = true;
         }
     }
 }
